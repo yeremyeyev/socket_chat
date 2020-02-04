fdce8ce757e2c3b104302a9b4d312b89f760e2a8
Eremeev Andrey <a.yeremyeyev@gmail.com>
2020-02-04 15:55:18 +0200

    04/02/2020

--- socket/app.js
+++ socket/app.js
@@ -51,26 +51,65 @@
 //   console.log('Server at http://localhost:3000/');
 // });
 // _____4. Добавление пользовательских событий______
-let app = require("express")();
-let http = require("http").Server(app);
-let io = require("socket.io")(http);
+// let app = require("express")();
+// let http = require("http").Server(app);
+// let io = require("socket.io")(http);
 
-app.get("/", function(req, res) {
-  res.sendFile(__dirname + "/index.html");
+// app.get("/", function(req, res) {
+//   res.sendFile(__dirname + "/index.html");
+// });
+
+// io.on("connetion", function(socket) {
+//   console.log("A user connected");
+//   setTimeout(function() {
+//     // отправка своего события и обьекта клиенту
+//     socket.emit("myEvent", { description: "User event from server" });
+//   }, 2000);
+//   socket.on("disconnect", function() {
+//     console.log("A user disconnected");
+//   });
+// });
+// http.listen(3000, function() {
+//   console.log("Server at http://localhost:3000/");
+// });
+// ___5.Получение событий от клиента______
+let app = require('express')();
+let http = require('http').Server(app);
+let io = require('socket.io')(http);
+ 
+app.get('/', function(req, res){
+  res.sendFile(__dirname + 'index.html');
 });
-
-io.on("connetion", function(socket) {
-  console.log("A user connected");
-  setTimeout(function() {
-    // отправка своего события и обьекта клиенту
-    socket.emit("myEvent", { description: "User event from server" });
-  }, 2000);
-  socket.on("disconnect", function() {
-    console.log("A user disconnected");
-  });
+io.on('connection', function(socket){
+ socket.on('cllientEvent',  function(data) {
+   console.log(data);
+ });
 });
 http.listen(3000, function() {
-  console.log("Server at http://localhost:3000/");
+  console.log('Server at localhost:3000');
 });
 
-// 2:30:39
+// ___6 Широковещение - посыл сообщений всем подключенным клиентам__
+let app = require('express')();
+let http = require('http').Server(app);
+let io = require('socket.io')(http);
+ 
+app.get('/', function(req, res){
+  res.sendFile(__dirname + 'index.html');
+});
+ 
+let users = 0;
+io.on('connection', function(socket){
+  users++;
+  socket.broadcast.emit('brodcast', {description: users + ' users conncected'});
+  socket.on('disconnect', function (){
+    users--;
+    socket.broadcast.emit('broadcast', {description: users + ' users disconnected!'});
+  });
+});
+http.listen(3000, function(){
+  console.log('Server at localhost:3000');
+});
+
+_______________________________________________
+// 2:30:38
